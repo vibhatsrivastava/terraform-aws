045647a31bc58f8212738fe174e606f8ae0535c7
VIBHAT SRIVASTAVA <vibhat.sri13@gmail.com>
2025-05-21 20:35:20 +0530

    update

--- 1_create_ec2_instance.tf
+++ 1_create_ec2_instance.tf
@@ -1,18 +1,22 @@
-provider "aws" {
-  profile = var.profile_name
-}
+# provider "aws" {
+#   profile = var.profile_name
+# }
 
-resource "aws_instance" "terraform-ec2-1" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.public_key_name
-}
+# resource "aws_instance" "terraform-ec2-1" {
+#   ami           = var.ami
+#   instance_type = var.instance_type
+#   key_name      = var.public_key_name
+# }
 
-resource "aws_key_pair" "vsrivastava-ssh-public_key" {
-  key_name   = var.public_key_name
-  public_key = var.public_key_value
-}
+# resource "aws_key_pair" "vsrivastava-ssh-public_key" {
+#   key_name   = var.public_key_name
+#   public_key = var.public_key_value
+# }
 
-output "execution_result" {
-  value = aws_instance.terraform-ec2-1.id
+# output "execution_result" {
+#   value = aws_instance.terraform-ec2-1.id
+# }
+
+resource "null_resource" "task-1" {
+  
 }
